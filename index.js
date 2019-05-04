dee63acf26306dfc84b21b08b2fd5f7be5c9f57f
Mourad Kejji <mkejji@gmail.com>
2019-05-04 19:23:29 +0200

    [WIP] 	- Debug output 	- Account requests 	- Types and validation

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const config = require('./config.json');
 
 const client = new HttpClient(config);
 
-client.getAccounts().then((res) => {
-	console.log("Server said", res);
-}).catch((err) => {
-	console.error("Erreur");
-	console.error(err);
-});
+client.getAccounts()
+	.then((res) => console.log("Server said", res))
+	.catch((err) => {
+		console.error("Erreur");
+		console.error(err);
+	});
