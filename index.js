74f95c21a6d56454ed24d91fe2078cff4e457d60
Mourad Kejji <mkejji@gmail.com>
2019-05-11 18:22:42 +0200

    Fixed Content-Type header and added a few requests

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-const HttpClient = require('./lib/http-client');
+const HttpClient = require('./lib/http/httpClient');
 const config = require('./config.json');
-
+JSON.prettify = function(res) {
+	return this.stringify(res, null, 2);
+}
 const client = new HttpClient(config);
 
-client.getAccounts()
-	.then((res) => console.log("Server said", res))
+/*
+client.initCashIn({
+	partner_ref: 'CI-TEST-0002',
+	tag: 'Cash-in-test',
+	receiver_wallet_id: 'WE-0090627546358840',
+	fees_wallet_id: 'WF-4039830628867210',
+	amount: 100,
+	fees: 5,
+	return_url: 'http://devhunt.eu:4545',
+	lang: 'fr',
+	auth_timeout_delay: 86400
+})
+*/
+client.confirmCashIn('TX-8373472067548957')
+	.then((res) => console.log(JSON.prettify(res)))
 	.catch((err) => {
 		console.error("Erreur");
 		console.error(err);
